29ac4909aad1c913df473ce45c293dc3cd093b70
Jeremy K <jj.of.keeler@gmail.com>
2015-02-21 17:21:10 -0500

    add connection to db and JSON api call upon request

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+// Start up express and import routes
 var express = require('express');
 var server = express();
 var routes = require('./routes')(server);
 
+// Define the listening port and start the server
 var port = 3000;
 server.listen(port);
 console.log('Server is now listening on port ' + port);
--- routes/boardgame.js
+++ routes/boardgame.js
@@ -1,8 +1,28 @@
+// Database file location
+var db_file = './bgg.sqlite';
+// Import sqlite module
+var sqlite3 = require('sqlite3').verbose();
+// Connect to database and handle errors
+var db = new sqlite3.Database(db_file, function(error) {
+  if (!error) { console.log('SUCCESSFULLY connected to database at ' + db_file); }
+  else { console.log('ERROR connected to database at ' + db_file); }
+});
+// Temporary SQL statement
+var sql_stmt = 'SELECT objectname as game, rank, maxplayers, minplayers, playingtime from games order by random() limit 0, 10;';
+
+// Function to call when hitting the api for boardgameapp
 exports.api = function(req, res) {
+  // Check to see if request specific game or request random ones
   if (req.params.gameid) { res.send(req.params.gameid); }
-  else { res.send('Game API'); }
+  // If requesting random ones run SQL statement
+  else {
+    db.all(sql_stmt, function(err, information) {
+      res.send(information)
+    });
+  }
 }
 
+// Index function for boardgame app
 exports.index = function(req, res) {
   res.send("Welcome to the NodeJS version of the Boardgame App");
 }
