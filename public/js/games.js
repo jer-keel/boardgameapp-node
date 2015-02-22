aa210a119cf94300aac4a4734d02f404284a1243
Jeremy K <jj.of.keeler@gmail.com>
2015-02-21 21:15:59 -0500

    Update comments

--- public/js/games.js
+++ public/js/games.js
@@ -1,18 +1,26 @@
+// Create the angular app and add log
 var app = angular.module("gamesapp", []);
 console.log("AngularJS app had been loaded!");
 
+// Define a game using the GameClass
 var GameClass = function() {
   this.game = "A default game";
-  this.playingtime = "Unknown";
-  this.alert = function() {
-    alert(this.game + " :: " + this.playingtime + " minutes");
-  };
+  this.rank = "-1";
+  this.minplayers = "-1";
+  this.maxplayers = "-1";
+  this.playingtime = "-1";
 };
 
+// Create a GameController
 app.controller("GameController", ["$scope", "$http", function($scope, $http) {
+  // Initialize the scope games to none
   $scope.games = [];
+
+  // Request JSON information from the api
   $http.get("api/games").success(function(data, status, headers, config) {
     $scope.games = [];
+
+    // For each game object received from the api push it on to the games array
     angular.forEach(data, function(game) {
       var thisGame = new GameClass();
       angular.extend(thisGame, game);
